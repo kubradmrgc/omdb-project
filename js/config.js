1be14724333d0a2c1122533cb45e8a7743cbe5fb
Kübra Demirgüç <kubradmrgc965@gmail.com>
2026-05-16 22:53:05 +0300

    Add config.js for GitHub Pages deployment

---
+++ js/config.js
@@ -0,0 +1,5 @@
+/**
+ * Replace YOUR_API_KEY_HERE with your key from https://www.omdbapi.com/apikey.aspx
+ * This file is gitignored; for GitHub Pages, add your key before deploying.
+ */
+export const API_KEY = "f9f4c6cf";
